009fb44e3a2cf9d09c1c8db2ecb1e07b1592003d
sandeep lakhiwal <sandeeplakhiwal98@gmail.com>
2024-01-17 16:54:32 +0530

    changed to delopement mode

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 app.use(
   Cors({
-    origin: "https://instagramsocialmedia.vercel.app",
-    // origin: "http://localhost:5173",
+    // origin: "https://instagramsocialmedia.vercel.app",
+    origin: "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
